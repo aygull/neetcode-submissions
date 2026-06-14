a412164e1e437ea6a90677cf6a53ffa9f6af2a13
Айгуль <40753014+aygull@users.noreply.github.com>
2026-06-14 16:55:56 +0200

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    # Sorting (O(logn*n + mlogm))
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (len(s) != len(t)):
+            return False
+        return sorted(s) == sorted(t)
+
+    ## HashMap
+    #  def isAnagram(self, s: str, t: str) -> bool:
+    #     if (len(s) != len(t)):
+    #         return False
+    #     hashmapS, hashmapT = {}, {}
+
+    #     for i in range(len(s)):
+    #         hashmapS[s[i]] = 1 + hashmapS.get(s[i], 0)
+    #         hashmapT[t[i]] = 1 + hashmapT.get(t[i], 0)
+    #     return hashmapS == hashmapT
